b24037d2bca65d60f4502126db90ed78a8344025
Spartan30 <aaron.mcneil30@gmail.com>
2023-05-13 22:05:35 -0400

    Add playAgain and track results

--- TTT/Main.c
+++ TTT/Main.c
@@ -1,9 +1,46 @@
 #include "TicTacToe.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Results {
+    int draws;
+    int xWins;
+    int oWins;
+};
+
+void play(struct Results*);
 
 
 int main() {
+    struct Results *results = (struct Results *)malloc(sizeof(struct Results));
+    char playAgain = ' ';
 
+    results->draws = 0;
+    results->xWins = 0;
+    results->oWins = 0;
+
+    do {
+
+        play(results);
+
+        printf("Do you want to play again? (y/n)\n");
+        do {
+            scanf("%c", &playAgain);
+        }while(playAgain != 'y' && playAgain != 'Y' && playAgain != 'n' && playAgain != 'N');
+
+    }while(playAgain == 'y' || playAgain == 'Y');
+
+    printf("\n");
+    printf("There were %d draws\n", results->draws);
+    printf("X won %d game(s)\n", results->xWins);
+    printf("O won %d game(s)\n", results->oWins);
+    printf("\n");
+
+    free(results);
+    return 0;
+}
+
+void play(struct Results *results) {
     char board[10] = "000000000";
     int turn = 0;
     char currPlayer;
@@ -38,17 +75,18 @@ int main() {
     switch(winner) {
         case '0':
             printf("It's a draw!\n");
+            results->draws++;
             break;
         case '1':
             printf("X wins!\n");
+            results->xWins++;
             break;
         case '2':
             printf("O wins!\n");
+            results->oWins++;
             break;
         default:
             printf("Something went wrong :(\n");
             break;
     }
-
-    return 0;
 }
